bf57c294d146e23322681a8805b89267feaf8f33
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-03 11:38:45 +0000

    omniauth correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,25 @@
 class User < ActiveRecord::Base
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
   has_many :reviews
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
  def self.from_omniauth(auth)
-   where(provider: auth_provider, uid: auth.uid).first_or_create do |user|
+   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
      user.email = auth.info.email
-     user.password = Devise.friendly_token[0, 20]
-     user.name = auth.info.name
-     user.image = auth.info.image
+     user.password = Devise.friendly_token[0,20]
    end
  end
+
+ def self.new_with_session(params, session)
+   super.tap do |user|
+     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+       user.email = data["email"] if user.email.blank?
+     end
+   end
+ end
+
+
 end
