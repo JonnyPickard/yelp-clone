55220aa7654028bffc3d8f565a334b8f2a0dffb0
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-03 08:58:41 +0000

    association of users, restaurants and reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,8 @@
 class Restaurant < ActiveRecord::Base
+  # belongs_to :user
+
   has_many :reviews, dependent: :destroy
+
 
   validates :name, length: {minimum: 3}, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :reviewed_restaurants, through: :reviews, source: :restaurant
+  has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
