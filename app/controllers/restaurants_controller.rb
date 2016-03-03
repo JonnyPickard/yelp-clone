4b36013ea170af5f75e5a32dfa57265f39e02908
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-03 15:31:10 +0000

    User can only delete restaurants they have created

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -41,8 +41,12 @@ class RestaurantsController < ApplicationController
 
   def destroy
     @restaurant = Restaurant.find(params[:id])
-    @restaurant.destroy
-    flash[:notice] = 'Restaurant deleted successfully'
+    if @restaurant.user_id == current_user.id
+      @restaurant.destroy
+      flash[:notice] = 'Restaurant deleted successfully'
+    else
+      flash[:notice] = 'This is not your restaurant'
+    end
     redirect_to '/restaurants'
   end
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -90,13 +90,9 @@ feature 'restaurants' do
   context 'deleting restaurants' do
 
     before do
-      Restaurant.create name: 'KFC'
       visit('/')
-      click_link('Sign up')
-      fill_in('Email', with: 'test@example.com')
-      fill_in('Password', with: 'testtest')
-      fill_in('Password confirmation', with: 'testtest')
-      click_button('Sign up')
+      sign_up
+      create_restaurant
     end
 
     scenario 'removes a restaurant when a user clicks a delete link' do
